a8e20c1ebcb8b5008681e765c441b8b2a0ac61b6
Tim Wheeler <tim@timwheeler.com>
2018-05-31 22:30:12 -0400

    Lesson 4

--- src/app/index.js
+++ src/app/index.js
@@ -8,9 +8,15 @@ class TodoComponent extends React.Component{
             <div>
                 <h1>Hello, World</h1>
                 <p>Get Ready</p>
+                <p>{this.props.msg}</p>
+                <p><strong>Programming Language: </strong>{this.props.coding.language}</p>
+                <p><strong>Language Difficulty: </strong>{this.props.coding.difficulty}</p>
+                <p><strong>Language Developer: </strong>{this.props.coding.developer}</p>
             </div>
         );
     }
 }﻿
+
+var myCode = {language: 'React', difficulty: 'Medium', developer: 'Facebook'};
 // Put component into HTML page
-ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
+ReactDOM.render(<TodoComponent msg="I love coding." coding={myCode}/>, document.getElementById('todo-wrapper'));
